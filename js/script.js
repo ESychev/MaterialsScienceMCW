e9c292cb0eff68ef73c044d1241f8eee10d64069
ESychev <375298357706@ya.ru>
2023-03-03 09:00:00 +0300

    change emblema and print qr

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ function testHtml() {
     function test1() {
         var mark = 0;
 
-     if (document.testH.test1[0].checked) {
-    mark = 1;
-}
+        if (document.testH.test1[0].checked) {
+            mark = 1;
+        }
 
-return mark;
+        return mark;
     }
 
     function test2() {
@@ -27,19 +27,19 @@ return mark;
         return mark;
     }
 
-	function test3() {
+    function test3() {
         var mark = 0;
 
         var mark = 0;
 
-if (document.testH.test3[0].checked) {
-    mark = 1;
-}
+        if (document.testH.test3[0].checked) {
+            mark = 1;
+        }
 
-return mark;
+        return mark;
     }
-	
-	function test4() {
+
+    function test4() {
         var mark = 0;
 
         if (document.testH.test4[0].checked) {
@@ -48,8 +48,8 @@ return mark;
 
         return mark;
     }
-	
-	function test5() {
+
+    function test5() {
         var mark = 0;
 
         if (document.testH.test5[0].checked) {
@@ -58,7 +58,7 @@ return mark;
 
         return mark;
     }
-	
+
     function test6() {
         var mark = 0;
 
@@ -68,7 +68,8 @@ return mark;
 
         return mark;
     }
-	function test7() {
+
+    function test7() {
         var mark = 0;
 
         if (document.testH.test7[0].checked) {
@@ -77,7 +78,8 @@ return mark;
 
         return mark;
     }
-	function test8() {
+
+    function test8() {
         var mark = 0;
 
         if (document.testH.test8[0].checked) {
@@ -86,7 +88,8 @@ return mark;
 
         return mark;
     }
-      function test9() {
+
+    function test9() {
         var mark = 0;
 
         if (document.testH.test9[0].checked) {
@@ -95,7 +98,8 @@ return mark;
 
         return mark;
     }
-	function test10() {
+
+    function test10() {
         var mark = 0;
 
         if (document.testH.test10[0].checked) {
@@ -104,14 +108,15 @@ return mark;
 
         return mark;
     }
+
     function test11() {
         var mark = 0;
 
-if (document.testH.test11[0].checked) {
-    mark = 1;
-}
+        if (document.testH.test11[0].checked) {
+            mark = 1;
+        }
 
-return mark;
+        return mark;
     }
 
     function test12() {
@@ -123,7 +128,8 @@ return mark;
 
         return mark;
     }
-	function test13() {
+
+    function test13() {
         var mark = 0;
 
         if (document.testH.test13[0].checked) {
@@ -132,7 +138,8 @@ return mark;
 
         return mark;
     }
-	    function test14() {
+
+    function test14() {
         var mark = 0;
 
         if (document.testH.test14[0].checked) {
@@ -141,7 +148,8 @@ return mark;
 
         return mark;
     }
-	function test15() {
+
+    function test15() {
         var mark = 0;
 
         if (document.testH.test15[0].checked) {
@@ -150,27 +158,22 @@ return mark;
 
         return mark;
     }
-	
-    result = test1() + test2() + test3() + test4() + test5()+ test6() + test7() + test8() + test9()+ test10() + test11() + test12() + test13()+ test14() + test15();
+
+    result = test1() + test2() + test3() + test4() + test5() + test6() + test7() + test8() + test9() + test10() + test11() + test12() + test13() + test14() + test15();
     alert(" Количество верных ответов из 15  = " + result);
 
     document.getElementById('result_multi').innerHTML = result;
-      
+
 }
 
 
-$(function qrprint(){	
-	$(window).bind('beforeprint', function qrprint(){ 
-		if($("#print-qr").length == 0) {
-			$('body').append(
-				'<style>@media screen {#print-qr{display: none;}}</style>' + 
-				'<p id="print-qr"><img src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=H&chl=https://esychev.github.io/Diploma/index.html' + window.location.href + '"></p>'
-			);
-		}
-	});
+$(function qrprint() {
+    $(window).bind('beforeprint', function qrprint() {
+        if ($("#print-qr").length == 0) {
+            $('body').append(
+                '<style>@media screen {#print-qr{display: none;}}</style>' +
+                '<p id="print-qr"><img src="https://chart.googleapis.com/chart?cht=qr&chs=200x200&choe=UTF-8&chld=H&chl=https://esychev.github.io/MaterialsScienceMCW/index.html' + window.location.href + '"></p>'
+            );
+        }
+    });
 });
-
-
-
-
-
